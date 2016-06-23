94405c490d216f8ad183385c4512ce1b22cee6f0
leolower <leolower@gmail.com>
2016-06-23 10:45:41 -0300

    Get Pools from localStorage.

--- web/app/scripts/peer-service-mock.js
+++ web/app/scripts/peer-service-mock.js
@@ -8,10 +8,11 @@ function PeerService(cfg, UserService) {
   var PeerService = this;
 
   PeerService.getPools = function () {
-    return cfg.pools;
+    return getLS('pools') || cfg.pools;
   };
   PeerService.enterPool = function (pool) {
     pool.members.push(UserService.getUser().id);
+    saveLS('pools', cfg.pools);
   };
   PeerService.createPool = function (pool) {
     var countPools = cfg.pools.length;
@@ -21,15 +22,36 @@ function PeerService(cfg, UserService) {
     pool.id = 'pool' + (countPools + 1);
     cfg.pools.push(pool);
     cfg.triggers.push(pool.trigger);
+    saveLS('pools', cfg.pools);
   };
   PeerService.insurePool = function (new_pool, insureId) {
-    return _.forEach(cfg.pools, function (pool) {
+    _.forEach(cfg.pools, function (pool) {
       if (pool.id === new_pool.id) {
         pool.insures.push(insureId);
         pool.insurePremium += new_pool.insurePremium;
       }
     });
+    saveLS('pools', cfg.pools);
+    return;
   };
+
+  function getLS(key) {
+    var value;
+    var lsValue = localStorage.getItem(key)
+    if (lsValue) {
+      try {
+        value = JSON.parse(lsValue);
+      } catch (error) {
+        console.error(error.stack || error);
+      }
+    }
+    return value;
+  }
+
+  function saveLS(key, value) {
+    console.log('saving', JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value));
+  }
 
 }
 
